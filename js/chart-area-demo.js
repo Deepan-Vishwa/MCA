10915d6ee677253294d72a2661fdf18c64cfb8be
Deepan Vishwa S R <srdeepansr@outlook.com>
2022-09-12 20:54:45 +0530

    initi

---
+++ js/chart-area-demo.js
@@ -0,0 +1,69 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+function number_format(number, decimals, dec_point, thousands_sep) {
+  // *     example: number_format(1234.56, 2, ',', ' ');
+  // *     return: '1 234,56'
+  number = (number + '').replace(',', '').replace(' ', '');
+  var n = !isFinite(+number) ? 0 : +number,
+    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+    s = '',
+    toFixedFix = function(n, prec) {
+      var k = Math.pow(10, prec);
+      return '' + Math.round(n * k) / k;
+    };
+  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+  if (s[0].length > 3) {
+    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+  }
+  if ((s[1] || '').length < prec) {
+    s[1] = s[1] || '';
+    s[1] += new Array(prec - s[1].length + 1).join('0');
+  }
+  return s.join(dec);
+}
+
+// Area Chart Example
+var ctx = document.getElementById("myAreaChart");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+      labels: ['j', 'f', 'm', 'a', 'm', 'j', 'jl'],
+      datasets: [
+          {
+          label: 'My First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          
+      },
+      {
+          label: 'My second Dataset',
+          data: [15, 29, 30, 41, 26, 15,20],
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          
+      },
+      {
+          label: 'My third Dataset',
+          data: [25, 19, 40, 21, 36, 25,30],
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          
+      }
+
+  ]
+  },
+  options: {
+      maintainAspectRatio: false,
+      tooltips: {
+          intersect: false,
+          mode: 'index',
+      },
+
+  }
+});
